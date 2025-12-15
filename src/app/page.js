10351b6066f7d1abddd9006427f55cb43268c7d4
Forman37 <forman3@Me.com>
2025-12-15 10:49:15 -0500

    adjusted for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
     return (
         <div className="bg-gray-500 h-screen">
             <Hero />
-            <div className="flex flex-row justify-center gap-2 bg-gray-500">
-                <div className="flex flex-col justify-center max-w-3xl">
+            <div className="flex flex-col md:flex-row justify-center gap-2 bg-gray-500">
+                <div className="flex flex-col justify-center max-w-2xl md:max-w-3xl">
                     <About />
                     <div className="flex flex-col gap-2 bg-gray-500">
                         <Projects />
                     </div>
                 </div>
-                <div className="flex flex-col max-w-lg">
+                <div className="flex flex-col max-w-2xl md:max-w-lg">
                     <ContactMe />
                     <Education />
                 </div>
