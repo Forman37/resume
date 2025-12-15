c4c21008aff7377e71cb08215745c4c35b531726
Forman37 <forman3@Me.com>
2025-12-15 13:01:50 -0500

    Added new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 // src/data/projects.js
 import AsuClubsImg from "@/assets/images/projects/asu-clubs.png";
 import PaycheckerImg from "@/assets/images/projects/paychecker-screens-1.png";
+import AsuBookStoreImg from "@/assets/images/projects/asu-bookstore.png";
 
 const projects = [
     {
@@ -19,6 +20,14 @@ const projects = [
         tech: ["Swift", "Firestore", "Firebase"],
         image: PaycheckerImg,
         appStore: "https://apps.apple.com/us/app/paychecker/id6751362951",
+    },
+    {
+        id: "asu-bookstore",
+        title: "ASU BookStore Web App",
+        short: "A collaborated full-stack app for ASU bookstore with SQLite database, admin access, and cart tracking. Parts completed by me are the home page, database setup, and navigation logic. Some input through the rest of the pages.",
+        tech: ["Java", "CSS", "SQLite"],
+        image: AsuBookStoreImg,
+        github: "https://github.com/Forman37/ASU-Library-Project/tree/main",
     },
 ];
 
